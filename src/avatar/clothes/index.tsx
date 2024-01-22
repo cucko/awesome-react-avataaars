67dde3f80e21e05050391172bb2049351dbb8565
Gorancho <gorancho@converz.co>
2024-01-22 09:23:05 +0100

    revert changes

--- src/avatar/clothes/index.tsx
+++ src/avatar/clothes/index.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react'
 
+import { ClotheOption, Selector } from '../../options'
+import { PieceProps } from '../piece'
 import BlazerShirt from './BlazerShirt'
 import BlazerSweater from './BlazerSweater'
 import CollarSweater from './CollarSweater'
-//import GraphicShirt from './GraphicShirt'
-import { ClotheOption, Selector } from '../../options'
-import { PieceProps } from '../piece'
+import GraphicShirt from './GraphicShirt'
 import Hoodie from './Hoodie'
 import Overall from './Overall'
 import ShirtCrewNeck from './ShirtCrewNeck'
@@ -22,7 +22,7 @@ export default class Clothes extends React.Component<PieceProps> {
         <BlazerShirt />
         <BlazerSweater />
         <CollarSweater />
-        {/* <GraphicShirt /> */}
+        <GraphicShirt />
         <Hoodie />
         <Overall />
         <ShirtCrewNeck />
